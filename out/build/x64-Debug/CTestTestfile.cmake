4a9352e4abdb21888518ee8e69d12bc5328c8100
Chens <87771978+ThatGuyChens@users.noreply.github.com>
2023-03-06 17:19:20 +0200

    Fix?

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Chens/Documents/Github/SDL-TOWER
+# Build directory: C:/Users/Chens/Documents/Github/SDL-TOWER/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
