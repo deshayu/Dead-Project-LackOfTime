59d73377bfb0b7abdf12a00ee4089af31357769f
Chens <87771978+ThatGuyChens@users.noreply.github.com>
2023-03-11 15:51:32 +0200

    Cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Chens/Documents/Git/Pizza Tower/SDL-Tower
-# Build directory: /home/Chens/Documents/Git/Pizza Tower/SDL-Tower/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
